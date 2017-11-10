26058c33847c57d2273171531312fa3260db9e03
BalKrishnaDhakal <bdhakal1@my.centennialcollege.ca>
2017-11-09 20:29:58 -0500

    Tested shape class

--- practice/AbstractShape(Ploymorphism)/src/ShapeTest.java
+++ practice/AbstractShape(Ploymorphism)/src/ShapeTest.java
@@ -9,7 +9,7 @@ public class ShapeTest {
 		System.out.println(s1.getPerimeter());     // which version?
 		System.out.println(s1.getColor());
 		System.out.println(s1.isFilled());
-		System.out.println(s1.getRadius());
+	//	System.out.println(s1.getRadius());
 		   
 		Circle c1 = (Circle)s1;                   // Downcast back to Circle
 		System.out.println(c1);
@@ -19,14 +19,14 @@ public class ShapeTest {
 		System.out.println(c1.isFilled());
 		System.out.println(c1.getRadius());
 		   
-		Shape s2 = new Shape();
+	//	Shape s2 = new Shape();
 		   
 		Shape s3 = new Rectangle(1.0, 2.0, "RED", false);   // Upcast
 		System.out.println(s3);
 		System.out.println(s3.getArea());
 		System.out.println(s3.getPerimeter());
 		System.out.println(s3.getColor());
-		System.out.println(s3.getLength());
+	//	System.out.println(s3.getLength());
 		   
 		Rectangle r1 = (Rectangle)s3;   // downcast
 		System.out.println(r1);
@@ -38,7 +38,7 @@ public class ShapeTest {
 		System.out.println(s4);
 		System.out.println(s4.getArea());
 		System.out.println(s4.getColor());
-		System.out.println(s4.getSide());
+	//	System.out.println(s4.getSide());
 		  
 		// Take note that we downcast Shape s4 to Rectangle, 
 	 // which is a superclass of Square, instead of Square
